eb535979c1c9964a8aa253b3eabe24168aabb6a6
Jorrit Poelen <jhpoelen@gmail.com>
2014-04-29 21:04:52 -0700

    make it succeed again

--- inst/tests/test-fakepackage.r
+++ inst/tests/test-fakepackage.r
@@ -5,7 +5,7 @@ test_that("returns 3", {
 })
 
 test_that("is_three recognizes 3", {
-  expect_that(is_three(4), is_true())
+  expect_that(is_three(3), is_true())
   expect_that(is_three(5), not(is_true()))
   expect_that(is_three(5), is_false())
 })
