bcd3b1e2527adab8c0fecf5a422435939eca7bc5
Jorrit Poelen <jhpoelen@gmail.com>
2014-05-30 11:28:45 -0700

    added general purpose cypher query method

--- R/globi.R
+++ R/globi.R
@@ -1,5 +1,10 @@
-GetInteractions <- function(type = 'preysOn') {
-  read.csv(text = httr::content(httr::GET('http://api.globalbioticinteractions.org/taxon/Homo%20sapiens/preysOn?type=csv')))
+GetGloBIURL <- function(suffix) {
+  paste("http://api.globalbioticinteractions.org", suffix, sep = "")
+}
+
+GetInteractions <- function(type = "preysOn") {
+  requestURL = GetGloBIURL("/taxon/Homo%20sapiens/preysOn?type=csv")
+  read.csv(text = httr::content(httr::GET(requestURL)))
 }
 
 GetPreyOf <- function() {
@@ -10,4 +15,26 @@ GetPreyOf <- function() {
   # Returns:
   #   List of prey names.
   GetInteractions()
+}
+
+Query <- function(querystring) {
+  # Executes Cypher Query against GloBI's Neo4j instance
+  #
+  # Args:
+  #   Cypher query
+  # Returns:
+  #	  Cypher query results
+
+  h <- basicTextGatherer()
+
+  curlPerform(
+    url=GetGloBIURL(":7474/db/data/ext/CypherPlugin/graphdb/execute_query"),
+    postfields=paste('query',curlEscape(querystring), sep='='),
+		writefunction = h$update,
+		verbose = FALSE
+  )
+  result <- fromJSON(h$value())
+  data <- data.frame(t(sapply(result$data, unlist)))
+  names(data) <- result$columns
+  data
 }
