d0e174a5a723866bcf8e3fcc4be39b5327cb7ca0
Jorrit Poelen <jhpoelen@gmail.com>
2014-05-30 11:36:02 -0700

    fix test error

--- tests/testthat/test-globi.R
+++ tests/testthat/test-globi.R
@@ -6,7 +6,7 @@ test_that("default prey", {
   expect_that(length(predatorPrey$source_taxon_name)>10, is_true())
 })
 
-that_that("cypher query", {
+test_that("cypher query", {
   human <- Query("START taxon = node:taxons(name='Homo sapiens') RETURN taxon.name as `name`, taxon.path as `path`");
   expect_that(human.name, is_equal('Homo sapiens'))
   expect_that(human.path, is_equal('donald duck'))
