9210d518b074b4bf8e053d6bd5dc84571a4290fc
J. Stephen Gosnell <stephengosnell@gmail.com>
2014-06-07 23:58:44 -0400

    added tests for get_interactions_by_taxa function and bounding box checks

--- tests/testthat/test-globi.R
+++ tests/testthat/test-globi.R
@@ -21,3 +21,26 @@ test_that("cypher query", {
   expect_equal(as.character(human$name), "Homo sapiens")
   expect_equal(as.character(human$path), "Animalia | Chordata | Mammalia | Primates | Hominidae | Homo | Homo sapiens")
 })
+
+test_that("interactions returned based on species", {
+  rattus=get_interactions_by_taxa(sourcetaxon = "Rattus rattus")
+  expect_equal(as.character(rattus$source_taxon_name[1]), "Rattus rattus")
+  expect_equal(as.character(rattus$source_taxon_path[1]), "Animalia | Chordata | Mammalia | Rodentia | Muridae | Rattus | Rattus rattus")
+})
+
+test_that("interactions subsetted by adding additional information", {
+  rattus=get_interactions_by_taxa(sourcetaxon = "Rattus rattus")
+  rattusaves=get_interactions_by_taxa(sourcetaxon = "Rattus rattus", targettaxon="Aves")
+  expect_less_than(dim(rattusaves)[1], dim(rattus)[1])
+  expect_equal(dim(merge(rattusaves,rattus, all.x=T, all.y=T)), dim(rattus))
+})
+
+test_that("bad bouding box throws error", {
+  throws_error(get_interactions_by_taxa(sourcetaxon = "Rattus rattus", bbox=c(23,35,22,50)))
+  throws_error(get_interactions_by_taxa(sourcetaxon = "Rattus rattus", bbox=c(23,35,22)))
+  throws_error(get_interaction_areas(bbox=c(23,35,22,50)))
+  throws_error(get_interactions_in_area(bbox=c(23,35,22,50)))
+})
+
+
+
