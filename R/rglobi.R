66c75ce8f3422fca2df4f114bcdd80fb14960a56
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-10-03 12:08:00 -0500

    remove cypher support

--- R/rglobi.R
+++ R/rglobi.R
@@ -1,6 +1,5 @@
 
 globi_api_url <- "api.globalbioticinteractions.org"
-globi_neo4j_url <- "neo4j.globalbioticinteractions.org"
 
 # Get GloBI Options
 
@@ -28,13 +27,6 @@ get_globi_url <- function(suffix, opts = list()) {
 # @return true if api is available, false otherwise 
 has_api <- function() {
   !is.null(curl::nslookup(globi_api_url, error = FALSE))
-}
-
-# Check If Neo4J API available
-#
-# @return true if neo4j api is resolvable, false otherwise
-has_neo4j_api <- function() {
-  !is.null(curl::nslookup(globi_neo4j_url, error = FALSE))
 }
 
 # Read csv URL
@@ -112,54 +104,6 @@ get_prey_of <- function(taxon = "Homo sapiens", ...) {
 get_predators_of <- function(taxon = "Rattus rattus", ...) {
   get_interactions(taxon, "preyedUponBy", ...)
 }
-
-cypher_result_as_dataframe <- function(result) {
-  nullToNA <- function(x) {
-    ifelse(is.null(x), NA, x)
-  }
-  f <- function(accum, x) {
-	  row <- data.frame(lapply(x, nullToNA), stringsAsFactors=FALSE)
-	  # give rows temporary and consist names for easy of combining
-    names(row) <- 1:length(x)
-    rbind(accum, row)
-  }
-  df <- Reduce(f, result$data, init=data.frame())
-  if (length(df) > 0) {
-    names(df) <- result$columns
-  }
-  df
-}
-
-#' Executes a Cypher Query Against GloBI's Neo4j Instance
-#'
-#' @import RCurl
-#' @param cypherQuery Cypher query (see http://github.com/globalbioticinteractions/globalbioticinteractions/wiki/cypher for examples)
-#' @param opts list of named options to configure GloBI API
-#' @return result of cypher query string
-#' @export
-query <- function(cypherQuery, opts = list()) {
-  if (has_neo4j_api()) {
-    h <- RCurl::basicTextGatherer()
-    opts_cypher <- add_missing_options(opts, host = globi_neo4j_url)
-    RCurl::curlPerform(
-      url=get_globi_url("/db/data/cypher", opts_cypher),
-      postfields=rjson::toJSON(list('query' = cypherQuery)),
-                httpheader=c(Accept="application/json",
-                           'Content-Type' = "application/json"),
-		writefunction = h$update,
-		verbose = FALSE
-    )
-    result <- rjson::fromJSON(h$value())
-    if (is.null(result$message)) {
-      cypher_result_as_dataframe(result)
-    } else {
-      stop(result$message)
-    }
-  } else {
-    stop(paste("GloBI Neo4j data services not available at [", globi_neo4j_url, "]. Are you connected to the internet?", sep = ""))
-  }
-}
-
 
 create_bbox_param <- function(bbox) {
   if (is.null(bbox)){
@@ -432,63 +376,6 @@ get_interaction_matrix <- function(source.taxon.names = list('Homo sapiens'), ta
   Reduce(function(accum, source.taxon.name) rbind(accum, unique_target_taxa_of_source_taxon(source.taxon.name, target.taxon.names, interaction.type, opts = opts, read_csv = read_csv)), source.taxon.names, init=data.frame())
 }
 
-#' Returns all known child taxa with known interaction of specified taxa and rank.
-#'
-#' @param taxon.names list of taxa of which child taxa should be included.
-#' @param rank selected taxonomic rank of child taxa
-#' @param skip number of child taxon names to skip before returning result. May be used for pagination.
-#' @param limit maximum number of child taxon names returned
-#' @param opts list of options including web service configuration like "port" and "host"
-#' @return list of child taxon names
-#' @family interactions
-#' @export
-#' @examples \dontrun{
-#' get_child_taxa(list("Aves"))
-#' }
-get_child_taxa <- function(taxon.names, rank = 'Species', skip = 0, limit = 25, opts = list()) {
-  luceneQuery <- paste('path:', taxon.names, ' ', sep='', collapse='')
-  cypher <- paste("START taxon = node:taxonPaths('", luceneQuery , "') WHERE has(taxon.rank) AND taxon.rank = '", rank, "' RETURN distinct(taxon.name) as `taxon.name` SKIP ", skip, " LIMIT ", limit, sep="")
-  query(cypher, opts = opts)$taxon.name
-}
-
 interaction_id_for_type <- function(interaction.type) {
   list(preysOn = 'RO_0002439', parasiteOf = 'RO_0002444', pollinates = 'RO_0002455')[[interaction.type]]
-}
-
-
-#' Returns all known child taxa with known interaction of specified source and target taxa on any rank.
-#'
-#' @param source.taxon.names list of taxon names for source
-#' @param target.taxon.names list of taxon names for target
-#' @param interaction.type kind of interaction
-#' @param skip number of records skipped before including record in result table, used in pagination
-#' @param limit maximum number of interaction to include
-#' @param opts connection parameters and other options
-#' @return table of matching source, target and interaction types
-#' @family interactions
-#' @export
-#' @examples \dontrun{
-#' get_interaction_table(source.taxon.names = list("Aves"), target.taxon.names = list('Insecta'))
-#' }
-get_interaction_table <- function(source.taxon.names = list(), target.taxon.names = list(), interaction.type = "preysOn", skip = 0, limit = 100, opts = list()) {
-  interaction.id <- interaction_id_for_type(interaction.type)
-  rel_type <- rel_type_interaction_type(interaction.type)
-
-  start_clause = list()
-  if (length(source.taxon.names) > 0) {
-    sourceLuceneQuery <- paste('path:\\\"', source.taxon.names, '\\\" ', sep='', collapse='')
-    start_clause <- list(paste('taxon = node:taxonPaths(\"', sourceLuceneQuery, '\")', sep=''))
-  }
-  if (length(target.taxon.names) > 0) {
-    targetLuceneQuery <- paste('path:\\\"', target.taxon.names, '\\\" ', sep='', collapse='')
-    start_clause <- list(start_clause, paste('preyTaxon = node:taxonPaths(\"', targetLuceneQuery, '\")', sep=''))
-  }
-  if (length(start_clause) == 0) {
-    stop('must define source or target taxa or both')
-  }
-  start_clause <- paste('START', paste(unlist(start_clause), collapse=','))
-  match_clause <- paste(' MATCH sameTaxon2<-[:SAME_AS]-taxon<-[:CLASSIFIED_AS]-specimen-[:', rel_type, ']->prey-[:CLASSIFIED_AS]->preyTaxon-[:SAME_AS]->sameTaxon', sep='')
-  where_clause <- ' WHERE sameTaxon.externalId =~ \"OTT.*\" AND sameTaxon2.externalId =~ \"OTT.*\"'
-  cypher <- paste(start_clause, match_clause, where_clause, ' RETURN id(specimen) as `source_specimen_id`, taxon.name as `source_name`, sameTaxon2.externalId as `source_id`, \"', interaction.type, '\" as `interaction_name`, \"', interaction.id, '\" as `interaction_id`, id(prey) as `target_specimen_id`, preyTaxon.name as `target_name`, sameTaxon.externalId as `target_id` SKIP ', skip, ' LIMIT ', limit, sep='')
-  query(cypher, opts = opts)
 }
--- cran-comments.md
+++ cran-comments.md
@@ -1,6 +1,6 @@
 Dear Reviewers:
 
-I built this package using [R CMD build .] and checked it with command [R CMD check --as-cran rglobi_0.2.28.tar.gz] on my Ubuntu 20.04 LTS Focal Fossa (4.15.0-159-generic) machine running R version 4.2.1 (2022-06-23). 
+I built this package using [R CMD build .] and checked it with command [R CMD check --as-cran rglobi_0.3.0.tar.gz] on my Ubuntu 22.04.3 LTS machine running R version 4.2.1 (2022-06-23). 
 
 IMPROVEMENT
 * Adding warning message when query generates more results than can be returned. Big thanks for Helen Waters for their contributions @H-Waters https://github.com/H-Waters via https://github.com/ropensci/rglobi/pull/45
